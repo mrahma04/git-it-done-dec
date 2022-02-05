606fb574828c21159506517154318a40c8680269
Mohammad Rahman <mbrahman@gmail.com>
2022-02-05 16:23:40 -0500

    finished language buttons feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector('#user-form')
 var nameInputEl = document.querySelector('#username')
 var repoContainerEl = document.querySelector('#repos-container')
 var repoSearchTerm = document.querySelector('#repo-search-term')
+var languageButtonsEl = document.querySelector('#language-buttons')
 
 var getUserRepos = function (user) {
     var apiUrl = 'https://api.github.com/users/' + user + '/repos'
@@ -75,4 +76,29 @@ var displayRepos = function (repos, searchTerm) {
     }
 }
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues"
+
+    fetch(apiUrl).then(function(response){
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language)
+            })
+        } else {
+            alert('Error: GitHub user not found')
+        }
+    })
+}
+
+var buttonClickHandler = function(event) {
+
+    var language = event.target.dataset.language
+    
+    if (language) {
+        getFeaturedRepos(language)
+        repoContainerEl.textContent = ''
+    }
+}
+
 userFormEl.addEventListener('submit', formSubmitHandler)
+languageButtonsEl.addEventListener('click', buttonClickHandler)
