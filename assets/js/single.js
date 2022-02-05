483957c7fea763833676f64153821a03adb8bf3b
Mohammad Rahman <mbrahman@gmail.com>
2022-02-05 10:37:07 -0500

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,16 +1,36 @@
 var issueContainerEl = document.querySelector('#issues-container')
+var limitWarningEl = document.querySelector('#limit-warning')
+
+var repoNameEl = document.querySelector('#repo-name')
+
+var getRepoName = function() {
+    var queryString = document.location.search
+    var repoName = queryString.split("=")[1]
+
+    if (repoName) {
+        repoNameEl.textContent = repoName
+        getRepoIssues(repoName)
+    } else {
+        document.location.replace('./index.html')
+    }
+}
 
 var getRepoIssues = function (repo) {
 
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc"
 
     fetch(apiUrl).then(function (response) {
+        console.log(response)
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data)
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo)
+                }
             })
         } else {
-            alert('There was a problem with your request!')
+            document.location.replace('./index.html')
         }
     })
 }
@@ -47,4 +67,14 @@ var displayIssues = function (issues) {
     }
 }
 
-getRepoIssues('facebook/react')
+var displayWarning = function (repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit "
+
+    var linkEl = document.createElement('a')
+    linkEl.textContent = "See more issues on GitHub"
+    linkEl.setAttribute('href', "https://github.com/" + repo + "/issues")
+
+    limitWarningEl.append(linkEl)
+}
+
+getRepoName()
